f6df7aee4bcbb7b34df26fd8e3bf6be6574a5021
David Mayo <dam106uc@gmail.com>
2025-03-31 14:59:40 +0900

    Add migrations for book and author DB tables

---
+++ src/database/migrations/2025_03_31_000000_create_authors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->bigIncrements('id'); // primary key in Laravel 6
+            $table->string('name')->unique(); // assume each author name is unique
+            $table->timestamps(); // adds created_at and updated_at columns
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
---
+++ src/database/migrations/2025_03_31_100000_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id'); // primary key in Laravel 6
+            $table->string('title')->unique(); // assume each book title is unique
+            $table->unsignedBigInteger('author_id'); // Laravel 7+ would use foreignId()
+            $table->date('published_at')->nullable(); // nullable makes this field optional
+            $table->timestamps(); // adds created_at and updated_at columns
+
+            // define author_id as foreign key
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
