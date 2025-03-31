b1cf88bc2d01c273c15585716f24abe5752115f4
David Mayo <dam106uc@gmail.com>
2025-03-31 22:22:28 +0900

    Add factories for Book and Author models

---
+++ src/database/factories/BookFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Author;
+use App\Book;
+use Faker\Generator as Faker;
+
+$factory->define(Book::class, function (Faker $faker) {
+    return [
+        'title' => $faker->unique()->sentence,
+        'author_id' => factory(Author::class)->create(),
+        'published_at' => $faker->date
+    ];
+});
