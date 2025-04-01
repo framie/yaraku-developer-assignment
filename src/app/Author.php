5ec4691a2afd749ba7fc8e8bf74808ae0ee88636
David Mayo <dam106uc@gmail.com>
2025-04-01 15:31:35 +0900

    Update Book and Author model docstrings

--- src/app/Author.php
+++ src/app/Author.php
@@ -8,6 +8,11 @@ class Author extends Model
 {
     protected $fillable = ['name'];
 
+    /**
+     * Get the books associated with an author.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function books()
     {
         return $this->hasMany(Book::class);
--- src/app/Book.php
+++ src/app/Book.php
@@ -8,6 +8,11 @@ class Book extends Model
 {
     protected $fillable = ['title', 'author_id', 'published_at'];
 
+    /**
+     * Get the author associated with a book.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function author()
     {
         return $this->belongsTo(Author::class);
