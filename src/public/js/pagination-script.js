1b369aff7ed6abbd58f80f799b4a2eb461181089
David Mayo <dam106uc@gmail.com>
2025-04-04 13:28:08 +0900

    Updated pagination component to display page count instead of item count. Fixed issue with refreshPagination() function.

--- src/public/js/pagination-script.js
+++ src/public/js/pagination-script.js
@@ -3,16 +3,22 @@
 /**
  * Updates the pagination UI with new values.
  *
- * @param {number} from - The starting item number for the current page.
- * @param {number} to - The ending item number for the current page.
- * @param {number} total - The total number of items.
+ * @param {number} currentPage - The current page number.
+ * @param {number} lastPage - The last page number (i.e. the total number of pages).
  * @param {string|null} nextPageUrl - The URL for the next page, otherwise null.
  * @returns {void}
  */
-const refreshPagination = (from, to, total, nextPageUrl = None) => {
-    updateElementText('.pagination-from', from);
-    updateElementText('.pagination-to', to);
-    updateElementText('.pagination-total', total);
+const refreshPagination = (currentPage, lastPage, nextPageUrl = None) => {
+    updateElementText('.pagination-current-page', currentPage);
+    updateElementText('.pagination-last-page', lastPage);
+    const prevPageElement = document.querySelector('.pagination-prev');
+    if (prevPageElement) {
+        if (currentPage > 1) prevPageElement.classList.remove('disabled');
+        else prevPageElement.classList.add('disabled');
+    }
     const nextPageElement = document.querySelector('.pagination-next');
-    if (nextPageUrl && nextPageElement) nextPageElement.classList.remove('disabled');
+    if (nextPageElement) {
+        if (nextPageUrl) nextPageElement.classList.remove('disabled');
+        else nextPageElement.classList.add('disabled');
+    }
 }
--- src/resources/views/components/pagination.blade.php
+++ src/resources/views/components/pagination.blade.php
@@ -1,29 +1,30 @@
-<div class="pagination">
-    <button
-        type="button"
-        class="pagination-prev{{ $items->onFirstPage() ? ' disabled' : '' }}"
-        onclick="buttonHandler(this)"
-        data-key="page"
-        data-value="prev"
-    >
-        Previous
-    </button>
+<div class="pagination-container">
+    <div class="pagination panel">
+        <button
+            type="button"
+            class="pagination-prev{{ $items->onFirstPage() ? ' disabled' : '' }}"
+            onclick="buttonHandler(this)"
+            data-key="page"
+            data-value="prev"
+        >
+            < Previous
+        </button>
 
-    <span class="pagination-from">{{$from}}</span>
-    -
-    <span class="pagination-to">{{$to}}</span>
-    /
-    <span class="pagination-total">{{$items->total()}}</span>
+        <span class="pagination-text">Page</span>
+        <span class="pagination-text pagination-current-page">{{$currentPage}}</span>
+        <span class="pagination-text">of</span>
+        <span class="pagination-text pagination-last-page">{{$lastPage}}</span>
 
-    <button
-        type="button"
-        class="pagination-next{{ $items->hasMorePages() ? '' : ' disabled' }}"
-        onclick="buttonHandler(this)"
-        data-key="page"
-        data-value="next"
-    >
-        Next
-    </button>
+        <button
+            type="button"
+            class="pagination-next{{ $items->hasMorePages() ? '' : ' disabled' }}"
+            onclick="buttonHandler(this)"
+            data-key="page"
+            data-value="next"
+        >
+            Next >
+        </button>
+    </div>
 </div>
 
 <script src="{{ asset('js/pagination-script.js') }}"></script>
