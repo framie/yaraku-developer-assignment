8738243e39cc4d03a38d739776e26ef812f6b621
David Mayo <dam106uc@gmail.com>
2025-04-04 16:49:32 +0900

    Added global styling sheet and ensures that stylesheets are being loaded in the layout file.

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>@yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+        @stack('styles')
     </head>
     <body>
         <main>
