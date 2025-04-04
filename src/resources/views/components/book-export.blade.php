805a485684694f79fc9111292bda022ce2cdc349
David Mayo <dam106uc@gmail.com>
2025-04-04 16:51:54 +0900

    Refactored the book export component to initially show a button which will then display a modal with the export options.

--- src/resources/views/components/book-export.blade.php
+++ src/resources/views/components/book-export.blade.php
@@ -1,18 +1,54 @@
-<form id="book-export-form" action="{{ route('books.index') }}">
-    <label for="type">Data:</label>
-    <select name="type">
-        <option value="all">Book Titles and Author Names</option>
-        <option value="titles">Book Titles only</option>
-        <option value="authors">Author Names only</option>
-    </select>
-    
-    <label for="format">Format:</label>
-    <select name="format">
-        <option value="csv">CSV</option>
-        <option value="xml">XML</option>
-    </select>
+<button type="button" class="button" onclick="openModal('book-export-modal')">
+    Export data
+</button>
 
-    <button type="button" onclick="exportHandler();">Export Data</button>
-</form>
+<div
+    id="book-export-modal"
+    class="book-export-modal modal--container fade"
+    onclick="closeModal('book-export-modal', event)"
+>
+    <div class="modal">
+        <button
+            class="modal__close"
+            onclick="closeModal('book-export-modal')"
+            type="button"
+        >
+            X
+        </button> 
+        <form id="book-export-form" class="modal__form" action="{{ route('books.index') }}">
+            <span class="modal__title">Export Data</span>
+            <div class="modal__form-group">
+                <label class="modal__form-label" for="type">Data:</label>
+                <select class="select" title="type" name="type">
+                    <option value="all">Books and Authors</option>
+                    <option value="titles">Book Titles only</option>
+                    <option value="authors">Author Names only</option>
+                </select>
+            </div>
+                
+            <div class="modal__form-group">
+                <label class="modal__form-label" for="format">Format:</label>
+                <select class="select" title="format" name="format">
+                    <option value="csv">CSV</option>
+                    <option value="xml">XML</option>
+                </select>
+            </div>
+
+            <div class="modal__form-group">
+                <button
+                    class="button button--confirm"
+                    type="button"
+                    onclick="exportHandler();"
+                >
+                    Export Data
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 
 <script src="{{ asset('js/book-export-script.js') }}"></script>
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/modal.css') }}">
+@endpush
